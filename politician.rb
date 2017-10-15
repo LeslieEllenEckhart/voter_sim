4ca18f8729b1ad1ea47c2616c78f52c16cfb2f27
Leslie Eckhart <leslie.ellen.eckhart@gmail.com>
2017-10-15 19:48:52 +0000

    Politician workds for init only

--- politician.rb
+++ politician.rb
@@ -1,8 +1,33 @@
-# Class for a "politician" for voter simulation
+# Rubyist:  Leslie E. Eckhart
+# I-O Avenue, Highway - Fall 2017 cohort
+# Project-3: Voter Simulation
 # 
+# Politician class object for voter simulation:
+# > Politician is a child of the Person class
+# > must be able to read and write political_affiliation
+#   
+
 class Politician < Person
     
     attr_accessor :political_affiliation
+      
+    def initialization(name, party)
+        if name == " " 
+            raise ArgumentError
+        else
+            @politician_name = name
+            @@name = @politician_name
+        end
+        
+        if party == " "
+            @political_affiliation = "None"
+        else
+            @political_affiliation = party
+        end
+        
+    end
+    
+    
     
     
     
--- spec/politician_spec.rb
+++ spec/politician_spec.rb
@@ -1,10 +1,22 @@
-# this is taking the file holding Politician code for here.
+# Rubyist:  Leslie E. Eckhart
+# I-O Avenue, Highway - Fall 2017 cohort
+# Project-3: Voter Simulation
+# 
+# Test for Politician class object.
+
 require './politician'
-# describe the classname
+
 describe Politician do
-    it "is a Person"
-    it "has a political affiliation"
-    it "can report its political affiliation"
-    it "can update its political affiliation"
+    it "is created with a name and political affiliation" do
+        politician = Politician.new("Democrat")
+        expect(politician).to be_a(Person)
+        expect(politician).to be_a(Politician)
+    end
+    
+    it "can report its political affiliation" do
+    end
+    
+    it "can update its political affiliation" do
+    end
     
 end
